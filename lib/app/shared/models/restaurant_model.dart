c47fcbee13a78d7a48cd6746157092040ce95dd6
Diego <diegolima362@gmail.com>
2020-11-29 17:06:38 -0300

    update models

--- lib/app/shared/models/restaurant_model.dart
+++ lib/app/shared/models/restaurant_model.dart
@@ -1,60 +1,135 @@
 import 'package:coffee_tracker/app/shared/models/review_model.dart';
+import 'package:coffee_tracker/app/utils/format.dart';
 import 'package:flutter/foundation.dart';
 
 class RestaurantModel {
-  final int id;
-
-  double rate;
-
+  final String id;
   String name;
+  String commentary;
   String address;
   String city;
   String state;
-
-  String openDays;
-
-  String photoURL;
+  bool favorite;
+  String fileName;
+  final DateTime registerDate;
 
   List<ReviewModel> _reviews;
 
+  double get rate => _reviews.isEmpty
+      ? 0
+      : _reviews.fold(0, (v, r) => v += r.rate) / _reviews.length;
+
   RestaurantModel({
     @required this.id,
+    @required this.registerDate,
     @required this.name,
-    @required this.address,
     @required this.city,
     @required this.state,
-    this.rate,
-    this.photoURL,
-    this.openDays,
+    this.address = '',
+    this.favorite = false,
+    this.fileName = '',
+    this.commentary = '',
   }) {
     _reviews = List<ReviewModel>();
   }
 
   List<ReviewModel> get allReviews => _reviews;
 
+  String getShareText(String userName) {
+    final text = StringBuffer(
+        '$userName acha que você vai gostar de ir ao ${Format.capitalString(name)}');
+
+    if (address.isNotEmpty) text.write('\nEm ${Format.capitalString(address)}');
+    text.write('\n${Format.capitalString(city)}, ${state.toUpperCase()}');
+
+    if (allReviews.isNotEmpty) text.write('\n⭐ $rate');
+
+    if (commentary.isNotEmpty) text.write('\n"$commentary"');
+
+    return text.toString();
+  }
+
   void addReview(ReviewModel review) => _reviews.add(review);
+
+  void addReviews(List<ReviewModel> reviews) => _reviews.addAll(reviews);
+
+  factory RestaurantModel.fromMap(
+      Map<String, dynamic> data, String documentId) {
+    if (data == null) return null;
+
+    return RestaurantModel(
+      id: data['id'] ?? '',
+      name: data['name'] ?? '',
+      address: data['address'] ?? '',
+      city: data['city'] ?? '',
+      state: data['state'] ?? '',
+      fileName: data['fileName'] ?? '',
+      favorite: data['favorite'] ?? false,
+      commentary: data['commentary'] ?? '',
+      registerDate: DateTime.fromMillisecondsSinceEpoch(
+          data['registerDate'] ?? DateTime.now().millisecondsSinceEpoch),
+    );
+  }
 
   factory RestaurantModel.fromJson(Map<String, dynamic> json) {
     return RestaurantModel(
-      id: json['id'],
-      name: json['name'],
-      address: json['address'],
-      city: json['city'],
-      state: json['state'],
-      photoURL: json['photoURL'],
-      openDays: json['openDays'],
-      rate: json['rate'],
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+      address: json['address'] ?? '',
+      city: json['city'] ?? '',
+      state: json['state'] ?? '',
+      fileName: json['fileName'] ?? '',
+      favorite: json['favorite'] ?? false,
+      commentary: json['commentary'] ?? '',
+      registerDate: DateTime.fromMillisecondsSinceEpoch(
+          json['registerDate'] ?? DateTime.now().millisecondsSinceEpoch),
     );
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
-        'address': address,
-        'city': city,
-        'state': state,
-        'photoURL': photoURL,
-        'openDays': openDays,
-        'rate': rate,
+        'address': address ?? '',
+        'city': city ?? '',
+        'state': state ?? '',
+        'fileName': fileName ?? '',
+        'favorite': favorite ?? false,
+        'commentary': commentary ?? '',
+        'registerDate': registerDate?.millisecondsSinceEpoch ??
+            DateTime.now().millisecondsSinceEpoch,
       };
+
+  @override
+  String toString() {
+    return '$name, $city, $state';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is RestaurantModel &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          commentary == other.commentary &&
+          address == other.address &&
+          city == other.city &&
+          state == other.state &&
+          favorite == other.favorite &&
+          fileName == other.fileName &&
+          registerDate == other.registerDate &&
+          _reviews == other._reviews;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      name.hashCode ^
+      commentary.hashCode ^
+      address.hashCode ^
+      city.hashCode ^
+      state.hashCode ^
+      favorite.hashCode ^
+      fileName.hashCode ^
+      registerDate.hashCode ^
+      _reviews.hashCode;
 }
--- lib/app/shared/models/review_model.dart
+++ lib/app/shared/models/review_model.dart
@@ -1,14 +1,17 @@
+import 'package:coffee_tracker/app/utils/format.dart';
 import 'package:flutter/foundation.dart';
 
 class ReviewModel {
-  final int restaurantId;
+  final String id;
+  final String restaurantId;
   final String restaurantName;
-  final String reviewDate;
-  final String visitDate;
+  final DateTime reviewDate;
+  DateTime visitDate;
   String text;
   double rate;
 
   ReviewModel({
+    @required this.id,
     @required this.restaurantName,
     @required this.restaurantId,
     @required this.reviewDate,
@@ -17,23 +20,73 @@ class ReviewModel {
     @required this.text,
   });
 
+  String getShareText(String userName) {
+    final review = StringBuffer(
+        '$userName compartilhou uma experiencia sobre ${Format.capitalString(restaurantName)}');
+
+    review.write('\n⭐ $rate');
+
+    if (text.isNotEmpty) review.write('\n"$text"');
+
+    return review.toString();
+  }
+
   factory ReviewModel.fromJson(Map<String, dynamic> json) {
+    final _reviewDate = DateTime.fromMillisecondsSinceEpoch(json['reviewDate']);
+    final _visitDate = DateTime.fromMillisecondsSinceEpoch(json['visitDate']);
+
     return ReviewModel(
+      id: json['id'],
       restaurantId: json['restaurantId'],
       restaurantName: json['restaurantName'],
-      reviewDate: json['reviewDate'],
-      visitDate: json['visitDate'],
+      reviewDate: _reviewDate,
+      visitDate: _visitDate,
       rate: json['rate'],
       text: json['text'],
     );
   }
 
   Map<String, dynamic> toJson() => {
+        'id': id,
         'restaurantName': restaurantName,
         'restaurantId': restaurantId,
-        'reviewDate': reviewDate,
-        'visitDate': visitDate,
+        'reviewDate': reviewDate.millisecondsSinceEpoch,
+        'visitDate': visitDate.millisecondsSinceEpoch,
         'rate': rate,
         'text': text,
       };
+
+  @override
+  String toString() {
+    return "{'id': $id,"
+        "'restaurantName': $restaurantName,"
+        "'restaurantId': $restaurantId,"
+        "'reviewDate': $reviewDate"
+        "'visitDate': $visitDate,"
+        "'rate': $rate,"
+        "'text': $text}";
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ReviewModel &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          restaurantId == other.restaurantId &&
+          restaurantName == other.restaurantName &&
+          reviewDate == other.reviewDate &&
+          visitDate == other.visitDate &&
+          text == other.text &&
+          rate == other.rate;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      restaurantId.hashCode ^
+      restaurantName.hashCode ^
+      reviewDate.hashCode ^
+      visitDate.hashCode ^
+      text.hashCode ^
+      rate.hashCode;
 }
